5b066b3d9a0ac97836b3cba303e466d0792e0465
Renzo Tan <renzotimtan@gmail.com>
2022-09-20 22:06:49 +0800

    text bugs

--- scripts.js
+++ scripts.js
@@ -7,9 +7,9 @@ const typewriter = new Typewriter(app, {
 typewriter.typeString("Hello World! I'm Renzo, a Software Engineer. 😎")
     .pauseFor(2500)
     .deleteAll()
-    .typeString('Welcome to my website!')
+    .typeString('Welcome to my handsome website!')
     .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>SHEESH</strong>')
+    .deleteAll()
+    .typeString('SHEESH')
     .pauseFor(2500)
     .start();
