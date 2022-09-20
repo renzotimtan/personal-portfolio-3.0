5aeecf81d1c5b095a210466c2e6fa52fcc8328fd
Renzo Tan <renzotimtan@gmail.com>
2022-09-20 21:27:28 +0800

    Finished portfolio

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const app = document.querySelector('.anim-typewriter');
+
+const typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString("Hello World! I'm Renzo, a Software Engineer. 😎")
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Welcome to my website!')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
